17c5c812e844f0ba3b3a17fcc16f9e7968061711
Alan Novicki <afnovicki@gmail.com>
2018-10-10 22:55:21 -0400

    Polishing user messages

--- shell-util/iowrapper.sh
+++ shell-util/iowrapper.sh
@@ -12,6 +12,9 @@ printf_green() { printf "$GREEN$*$NORMAL"; }
 printf_yellow() { printf "$YELLOW$*$NORMAL"; }
 printf_red() { printf "$RED$*$NORMAL"; }
 
+# $1: Prompt message (question)
+# $2: Affirmative response prints "YES - $2"
+# $3: Negative response prints "NO - $3"
 prompt() {
   printf_yellow "$1 [y/N]: " # [YES] or [NO] will appear on the same line for readability
 
@@ -24,10 +27,10 @@ prompt() {
 
   # did the user say yes? 0 = true
   if [[ $inputchar == "y" || $inputchar == "Y" ]]; then
-    printf_green "[YES]\n"
+    printf_green "[YES - $2]\n"
     return 0
   else
-    printf_red "[NO]\n"
+    printf_red "[NO - $3]\n"
     return 1
   fi
 }
